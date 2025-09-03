44e4470bd570a3273262fac7da5df166353c53f1
digeorgiou <digeorgiou1990@gmail.com>
2025-09-04 00:39:26 +0300

    -feat: fixing sql scripts

--- backend/src/main/resources/sql/01users.sql
+++ backend/src/main/resources/sql/01users.sql
@@ -1,10 +1,10 @@
-INSERT INTO users (username, password, role, is_active, created_at, updated_at, created_by_id, last_updated_by, uuid) VALUES
+INSERT INTO users (id, username, password, role, is_active, created_at, updated_at, created_by_id, last_updated_by, uuid) VALUES
 -- Admin user
-('admin', '$2a$11$fP8pczMMVsi/zGbJ2ZKDB.4hMaBl9pYSHdFFuTjQfLxE8/pcNfKa2', 'ADMIN', 1, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), NULL, NULL, UUID()),
+(1, 'admin', '$2a$11$fP8pczMMVsi/zGbJ2ZKDB.4hMaBl9pYSHdFFuTjQfLxE8/pcNfKa2', 'ADMIN', 1, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), NULL, NULL, UUID()),
 
 -- Regular users
-('maria', '$2a$11$fP8pczMMVsi/zGbJ2ZKDB.4hMaBl9pYSHdFFuTjQfLxE8/pcNfKa2', 'USER', 1, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), NULL, NULL, UUID()),
-('john', '$2a$11$fP8pczMMVsi/zGbJ2ZKDB.4hMaBl9pYSHdFFuTjQfLxE8/pcNfKa2', 'USER', 1, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), NULL, NULL, UUID()),
-('anna', '$2a$11$fP8pczMMVsi/zGbJ2ZKDB.4hMaBl9pYSHdFFuTjQfLxE8/pcNfKa2', 'USER', 1, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), NULL, NULL, UUID());
+(2, 'maria', '$2a$11$fP8pczMMVsi/zGbJ2ZKDB.4hMaBl9pYSHdFFuTjQfLxE8/pcNfKa2', 'USER', 1, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), NULL, NULL, UUID()),
+(3, 'john', '$2a$11$fP8pczMMVsi/zGbJ2ZKDB.4hMaBl9pYSHdFFuTjQfLxE8/pcNfKa2', 'USER', 1, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), NULL, NULL, UUID()),
+(4, 'anna', '$2a$11$fP8pczMMVsi/zGbJ2ZKDB.4hMaBl9pYSHdFFuTjQfLxE8/pcNfKa2', 'USER', 1, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), NULL, NULL, UUID());
 
 ALTER TABLE users AUTO_INCREMENT = 5;
