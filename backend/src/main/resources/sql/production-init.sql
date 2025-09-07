29e7e88b3adb6e9d919b516a01a411eaed6b746c
digeorgiou <digeorgiou1990@gmail.com>
2025-09-07 18:32:45 +0300

    fixing 7

--- backend/src/main/resources/sql/production-init.sql
+++ backend/src/main/resources/sql/production-init.sql
@@ -1,3 +1,3 @@
-INSERT INTO users (id, username, password, role, is_active, created_at, updated_at)
-VALUES (1, 'admin', '$2a$10$EixXVJA6v8a4m.rjSI0xBuNqK7C1kKmKqvfpPaUAOgNqJcJvOcT7u', 'ADMIN', true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+INSERT INTO users (id, username, password, role, is_active, uuid, created_at, updated_at)
+VALUES (1, 'admin', '$2a$10$EixXVJA6v8a4m.rjSI0xBuNqK7C1kKmKqvfpPaUAOgNqJcJvOcT7u', 'ADMIN', true, '550e8400-e29b-41d4-a716-446655440000', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
 ON CONFLICT (id) DO NOTHING;
