37ae39dc991f6ff08e29745d843a588858ee7e01
digeorgiou <digeorgiou1990@gmail.com>
2025-09-07 17:33:40 +0300

    fixing 3

--- backend/src/main/java/gr/aueb/cf/bluemargarita/rest/AuthenticationRestController.java
+++ backend/src/main/java/gr/aueb/cf/bluemargarita/rest/AuthenticationRestController.java
@@ -16,6 +16,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
